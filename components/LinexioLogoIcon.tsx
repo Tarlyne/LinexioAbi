3a0f2b00ce5c8b0b76ac99dd9ad485425321dcc2
Torben <thor0907@gmx.de>
2026-01-12 12:48:02 +0100

    feat: Update LinexioLogoIcon for LinexioAbi
    Refactor the LinexioLogoIcon component to incorporate the "Abi" element and use a gradient fill. This update better represents the LinexioAbi branding and adds visual appeal.

--- components/LinexioLogoIcon.tsx
+++ components/LinexioLogoIcon.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 interface LinexioLogoIconProps extends React.SVGProps<SVGSVGElement> {
@@ -5,46 +6,66 @@ interface LinexioLogoIconProps extends React.SVGProps<SVGSVGElement> {
 }
 
 /**
- * Linexio Branding Icon
- * Geometrisches Doppel-L Design für die Markenidentität.
+ * LinexioAbi Branding Icon
+ * Kombination aus der geometrischen Linexio-Form und dem Abitur-"A".
  */
 export const LinexioLogoIcon: React.FC<LinexioLogoIconProps> = ({ 
-  secondaryColor = '#06b6d4', 
   className = "w-7 h-7", 
   ...props 
 }) => (
   <svg 
     className={className} 
-    viewBox="50 30 65 75" 
+    viewBox="0 0 270.93333 270.93333" 
     fill="none" 
     xmlns="http://www.w3.org/2000/svg" 
     {...props}
   >
-    <g strokeLinecap="round" strokeLinejoin="round">
-      {/* Outer L - Akzentfarbe (Cyan) */}
+    <defs>
+      <linearGradient id="logoGradientAbi">
+        <stop offset="0.158" stopColor="#22d3ee" />
+        <stop offset="0.570" stopColor="#f59e0b" />
+      </linearGradient>
+      <linearGradient 
+        id="logoGradientAbiText" 
+        x1="117.68" y1="121.68" x2="166.11" y2="121.68" 
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0.158" stopColor="#22d3ee" />
+        <stop offset="0.570" stopColor="#f59e0b" />
+      </linearGradient>
+    </defs>
+    
+    <g>
+      {/* Geometrische Grundform (Linexio-Style) */}
       <path
-        stroke={secondaryColor}
-        strokeWidth="2.5"
-        d="m 54.971722,35.248072 0.127248,64.388173 54.84447,0.2545 L 94.928019,85.129819 H 69.859897 l 0.127249,-35.120823 z"
-        className="opacity-90"
+        stroke="#22d3ee"
+        strokeWidth="4.1"
+        fill="none"
+        d="m 62.584546,45.554174 0.328916,166.313296 141.764338,0.65737 -38.81249,-38.12716 h -64.79717 l 0.32891,-90.71636 z"
       />
       <path
-        stroke={secondaryColor}
-        strokeWidth="2.5"
-        d="M 94.800771,99.890745 54.844473,59.934448"
-        className="opacity-70"
+        stroke="#22d3ee"
+        strokeWidth="4.1"
+        fill="none"
+        d="M 165.53639,212.52484 62.255627,109.31859"
       />
-      {/* Inner L - Primärfarbe (weißlich/slate) */}
-      <path
-        stroke="currentColor"
-        strokeWidth="2.2"
-        d="m 66.492648,34.678024 0.101334,53.606363 43.675148,0.21188 -11.95746,-12.28921 H 78.348781 l 0.101335,-29.239829 z"
-      />
-      <path
-        stroke="currentColor"
-        strokeWidth="2.2"
-        d="M 98.716752,88.748345 66.902899,55.105832"
-      />
+      
+      {/* Das "A" für Abitur-Branding */}
+      <text
+        x="114.93772"
+        y="147.99986"
+        style={{
+          fontStyle: 'normal',
+          fontWeight: 'normal',
+          fontSize: '74.461px',
+          fontFamily: "'Comic Sans MS', cursive",
+          fill: 'none',
+          stroke: 'url(#logoGradientAbiText)',
+          strokeWidth: '4.0398'
+        }}
+      >
+        <tspan x="114.93772" y="147.99986">A</tspan>
+      </text>
     </g>
   </svg>
 );
